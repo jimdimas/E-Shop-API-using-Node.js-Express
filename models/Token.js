356ae5d4cfb96d8d98c9c86b2a40a37588fdb833
jimdimas <dimdimas@hotmail.gr>
2023-03-28 20:47:14 +0300

    created token model for authentication

---
+++ models/Token.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const TokenSchema = mongoose.Schema({
+    user:{
+        type:mongoose.Types.ObjectId,
+        ref:'User',
+        required:[true,'Please provide user id']
+    },
+    refreshToken:{
+        type:String,
+        unique:true,
+        required:[true,'Please provide refresh token']
+    },
+    ip:{
+        type:String,
+        unique:true,
+        required:[true,'Please provide ip address']
+    },
+    valid:{
+        type:Boolean,
+        default:true,
+    }
+},{timestamps:true})
+
+module.exports = mongoose.model('Token',TokenSchema)
