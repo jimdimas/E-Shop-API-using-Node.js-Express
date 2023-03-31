cd6d2eb5175f1d8a44dc218201a3dc395bbb961a
jimdimas <dimdimas@hotmail.gr>
2023-03-31 20:21:14 +0300

    fixed bug in errors

--- util/errors.js
+++ util/errors.js
@@ -1,30 +1,30 @@
-const {httpStatusCodes}=require('http-status-codes')
+const {StatusCodes}=require('http-status-codes')
 
 class BadRequestError extends Error{
     constructor(message){
         super(message)
-        this.statusCode = httpStatusCodes.BAD_REQUEST
+        this.statusCode = StatusCodes.BAD_REQUEST
     }
 }
 
 class UnauthenticatedError extends Error{
     constructor(message){
         super(message)
-        this.statusCode=httpStatusCodes.UNAUTHORIZED
+        this.statusCode=StatusCodes.UNAUTHORIZED
     }
 }
 
 class UnauthorizedError extends Error{
     constructor(message){
         super(message)
-        this.statusCode=httpStatusCodes.FORBIDDEN
+        this.statusCode=StatusCodes.FORBIDDEN
     }
 }
 
 class NotFoundError extends Error{
     constructor(message){
         super(message)
-        this.statusCode=httpStatusCodes.NOT_FOUND
+        this.statusCode=StatusCodes.NOT_FOUND
     }
 }
 
